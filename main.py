40a5b5b7ac44d2d58d89cf2c67d5f06d3fbcb715
zack <zackmcginnis@hotmail.com>
2020-03-10 21:16:39 -0700

    added todo's

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ from requests_oauthlib import OAuth1
 api_url = 'https://api.twitter.com/1.1/statuses/update.json'
 
 def select_tweet():
-    # get tweet from somewhere
+    # TODO: get tweet from somewhere (local text file for now)
+    # TODO: check if tweet has been used already (check tweet history of user? erase from text file?)
+        # TODO: keep list of used tweets in DB?
+    # TODO: check character limit
+    
     return 'this is a test tweet'
 
 
 # select a tweet 
 status = select_tweet()
 
-
-# check character limit
 
 # send tweet
 data_url = api_url + f"?status={quote_plus(status)}"
